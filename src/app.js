c8db6f5495083a0c7330f284259b148cd6782a93
Thigo Dev <thiagomvk08@gmail.com>
2024-04-01 14:09:29 -0300

    Criando funções auxiliares e rotas para post, get por id e delete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 
 const app = express();
 
+//indicar para o expresse ler body com json
+app.use(express.json());
+
 //mock
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
@@ -10,6 +13,17 @@ const selecoes = [
     {id: 4, selecao: 'Camarões', grupo: 'G'},
 ]
 
+//funções auxiliares
+//busca seleção pelo id
+const buscarSelecaoPorId = (id) => {
+    return selecoes.filter(sel => sel.id == id);
+}
+
+//busca o indice ou posição do elemento no array por id
+const buscarIndiceSelecao = (id) => {
+    return selecoes.findIndex(sel => sel.id == id);
+}
+
 //criar rota padrão ou  raiz
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -17,6 +31,21 @@ app.get('/', (req, res) => {
 
 app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes);
+});
+
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id));
+});
+
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body);
+    res.status(201).send('Seleção cadastrada com sucesso!');
+});
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndiceSelecao(req.params.id);
+    selecoes.splice(index, 1);
+    res.send(`Seleção com id: ${req.params.id} excluída com sucesso!`);
 });
 
 export default app;
